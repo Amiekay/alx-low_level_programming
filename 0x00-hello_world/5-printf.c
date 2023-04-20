53e92ca5cd21b342e936fd5ca8d05c25cacec13a
Amiekay <amara.kaluagu@gmail.com>
2023-04-20 16:15:34 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - using printf to print a C program
+ *
+ * Return 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
