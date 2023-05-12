1ffdba4e16223eeaa909f36a5511e72dda2d1c9e
Amiekay <amara.kaluagu@gmail.com>
2023-05-12 01:30:49 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints program's name
+ * Return: (0) for success
+ * @argc: argument count
+ * @argv: argument vector
+*/
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
